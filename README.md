0aca16e908b4042c005d91a44928ce98cbd5f85b
众触应用平台 <87555332+zclc@users.noreply.github.com>
2021-07-17 21:13:00 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# tooltip提示框
+
+众触零码应用平台插件
+
+Plugins For ZhongChu ZeroCode Application Platform
---
+++ plugins/zp139_tooltip提示框.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+function render({ children }) {
+    return children
+}
+
+function onInit({ exc, id, props }) {
+    exc('load(["https://unpkg.com/@popperjs/core@2.9.2/dist/umd/popper.min.js", "https://unpkg.com/tippy.js@6.3.1/dist/tippy-bundle.umd.min.js"])', {}, () => {
+        tippy("#" + id + "_0", {
+            content: props.content || "提示内容",
+            allowHTML: true,
+            interactive: true,
+        })
+    })
+}
+
+$plugin({
+    id: "zp139",
+    props: [{
+        prop: "content",
+        type: "text",
+        label: "内容",
+        ph: "可以是HTML"
+    }],
+    noContainer: true,
+    render,
+    onInit
+})
+
+/*
+https://github.com/popperjs/popper.js
+
+*/
