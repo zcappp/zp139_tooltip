de680d46bffa046276e1d43a1d6369019776f29d
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-02-13 08:48:40 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: process.env.mode || "development",
     devtool: false,
     entry: {
-        "zp139": "./plugins/zp139_tooltip提示框.js",
+        "zp139": "./src/zp139_tooltip提示框.js",
     },
     output: {
         path: path.join(__dirname, "build/"),
